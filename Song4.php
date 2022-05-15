93fdf85c6f8dfcd8442229cd275f0e9ffb10b6a0
kennbry449 <105537296+kennbry449@users.noreply.github.com>
2022-05-15 18:32:51 +0800

    Add files via upload

---
+++ Song4.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> SMART RECORDS </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to SMART RECORDS</h1>
+                <p class="lead">“Keep a song in your heart here in SMART RECORDS.” </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Simpleng Tao</h2>";
+                        echo "<p class='lead '>Gloc-9</p>";
+                        echo "<br/>";
+                
+
+                        echo "<p> [Verse 1]</p>";
+                        echo "<p> Habang tumutunog ang gitara sa 'kin, makinig ka sana Dumungaw ka sa bintana na parang isang harana</p>";
+                        echo "<p> Sa awit na aking i-sinulat ko kagabi'Wag sanang magmadali at 'wag kang mag-atubili dahil...</p>";
+                        echo "<p> Kahit na wala akong pera Kahit na butas aking bulsa</p>";
+                        echo "<p> Kahit pa maong ko'y kupas naAt kahit na marami d'yang iba</p>";
+                        echo "<p>Ganito man ako (maniwala ka sana sa akin)Simpleng tao (na ikaw ang lagi kong dalangin)</p>";
+                        echo "<p> Ang maipagyayabang ko lang sa 'yo (ay ang)</p>";
+                        echo "<br/>";
+
+                        //Chorus
+                        echo "<br/>";
+                        echo "<p> [Chorus]</p>";
+                        echo "<p> GPag-ibig ko sa 'yo na 'di magbabago At kahit na anong bagyo, ika'y masusundo</p>";
+                        echo "<p> Ganito lang ako (maniwala ka sana sa akin)Simpleng tao (na ikaw ang lagi kong dalangin) Na umaasa hanggang ngayon</p>";
+                        echo "<p> Hindi mo namang kailangan ang sagutinAng aking hinihiling Nais na maparating</p>";
+                        echo "<p> Na 'di na muli pang dadaloy ang luhaPupunasan nang kusa 'Di kailanagng manghula</p>";
+                        echo "<p> Kahit pamasahe lang ang palagi kong dalaUpang makasama ka Kapag nakikita ka</p>";
+                        echo "<p>Lagi kang aalalayan kahit ano man ang 'yong Mga ibinubulong Malalim pa sa balon Ito lamang ang...</p>";
+                        echo "<br/>";
+
+                        echo "<p> [Chorus X3]</p>";
+
+                        echo "<p> [Verse 2]</p>";
+                        echo "<p> Kay Spider-Man o kay Batman Kay Superman o Wolverine Kahit 'di maintindihan Baka sakaling pansinin</p>";
+                        echo "<p> Ganito lang ako (maniwala ka sana sa akin)Simpleng tao (na ikaw ang lagi kong dalangin) Ang maipagyayabang ko lang sa 'yo (ay ang)</p>";
+                        echo "<p> (Pag-ibig) Ko sa 'yo, ito'y totoo Wala nang iba, ikaw at ako Lang ang nasa isip at panaginip 'Pag nakikita ka, sasabihin ko'y</p>";
+                        echo "<p> Nawawala, ikaw na nga Ang dahilan kung bakit nasulat ko ang tulang Kahit kanino ay aking maipagyayabang Minamahal kita subalit tanggapin mo sana kahit...</p>";
+                        echo "<p> Ganito lang ako (maniwala ka sana sa akin)Simpleng tao (na ikaw ang lagi kong dalangin) Ang maipagyayabang ko lang sa 'yo (ay ang) </p>";
+                        echo "<br/>";
+
+
+                       //Chorus
+                        echo "<br/>";
+                        echo "<p> [Chorus]</p>";
+                        echo "<p> Pag-ibig ko sa 'yo na 'di magbabago At kahit na anong bagyo, ika'y masusundo Ganito lang ako (maniwala ka sana sa akin) Simpleng tao (na ikaw ang lagi kong dalangin) Ang maipagyayabang ko lang sa 'yo</p>";
+                        echo "<p> Pag-ibig ko sa 'yo na 'di magbabago At kahit na anong bagyo, ika'y masusundo Ganito lang ako (maniwala ka sana sa akin) Simpleng tao (na ikaw ang lagi kong dalangin) Na umaasa hanggang ngayon sa pag-ibig mo</p>";
+                        echo "<p> Sa pag-ibig mo Sa pag-ibig mo</p>";
+                        echo "<p> (Maniwala ka sana sa akin) (Na ikaw ang lagi kong dalangin)</p>";
+                        echo "<p> Sa pag-ibig mo (Maniwala ka sana sa akin) (Na ikaw ang lagi kong dalangin)</p>";
+                        echo "<p> Sa pag-ibig mo (Maniwala ka sana sa akin) (Na ikaw ang lagi kong dalangin)</p>";
+                        echo "<p> Sa pag-ibig mo (Maniwala ka sana sa akin) (Na ikaw ang lagi kong dalangin)</p>";
+
+
+                        echo "<br/>";
+                       
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Kenneth Bryan Ramiro </p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
